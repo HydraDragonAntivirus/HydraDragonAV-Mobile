849f7bc8d61c66a275f91e32c35653c527aa1c21
Emirhan Uçan <semaemirhan555@gmail.com>
2026-07-04 18:58:20 +0300

    Gradlew 9

--- app/src/main/java/com/hydradragon/antivirus/engine/UrlThreatScanner.java
+++ app/src/main/java/com/hydradragon/antivirus/engine/UrlThreatScanner.java
@@ -2,6 +2,7 @@ package com.hydradragon.antivirus.engine;
 
 import android.content.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
